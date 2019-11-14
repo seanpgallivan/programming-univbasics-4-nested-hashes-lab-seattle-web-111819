cd20cb7436a11b19badaf4204ae17e3886005f25
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-14 01:09:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,5 +124,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  "Assembly" << programmer_hash[:dennis_ritchie][:languages]
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
