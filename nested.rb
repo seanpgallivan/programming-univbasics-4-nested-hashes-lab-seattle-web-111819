d5e3a372d7f2ac096e1b3bb69a75604edf9be0ec
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-14 01:05:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,9 +78,13 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
+        },
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
         }
      }
-  programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
+#  programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
 end
 
 def changing_alan
